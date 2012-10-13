d452fc47738719c7b570ba2297af371c8d1ef529
andreipopescu <andrei_popescu@yahoo.com>
2012-10-13 15:43:00 +0300

    added Seed

---
+++ ShoppingHelper.DAL/ShoppingContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace ShoppingHelper.DAL
+{
+    public class ShoppingContext : DbContext
+    {
+
+        public ShoppingContext()
+            : base("ShoppingHelper")
+        {
+        }
+
+        public DbSet<Produs> Produse { get; set; }
+        public DbSet<Magazin> Magazine { get; set; }
+        public DbSet<MagazinProdus> MagazinProduse { get; set; }
+        public DbSet<Categorie> Categorii { get; set; }
+    }
+}
