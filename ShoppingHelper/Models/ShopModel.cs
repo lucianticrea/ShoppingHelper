01feab51ed943a590c137af585b3c70ef510c489
andreipopescu <andrei_popescu@yahoo.com>
2012-10-13 15:52:12 +0300

    update EF

---
+++ ShoppingHelper/Models/ShopModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace ShoppingHelper.DAL
+{
+    class ShopModel : DbContext
+    {
+       protected void Seed(ShoppingContext context)
+       {
+           GetCategorie().ForEach(c => context.Categorii.Add(c));
+           GetProdus().ForEach(p => context.Produse.Add(p));
+       }
+
+       private static List<Categorie> GetCategorie()
+       {
+           return new List<Categorie>();
+       }
+
+       private static List<Produs> GetProdus()
+       {
+           return new List<Produs>();
+       }
+    }
+}
